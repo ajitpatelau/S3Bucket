df9432cf9b94369ddac63f9ef1bc6885d5051120
ajitpatelau <ajitpatel@hotmail.com>
2022-01-11 10:59:51 +1100

    Added main terrafomr file to create S3 bucket

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "ajittestbucket" {
+    bucket = "ajfirstbucket"   
+    acl    = "public-read"
+  
+}
